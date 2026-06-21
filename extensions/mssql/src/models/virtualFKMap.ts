66c11e0bb401ba3e97c236a3b7df9afb020a13cc
fghdsfds <krkrogstad@gmail.com>
2026-06-21 10:48:20 +0200

    Added more items to dummyFKMap

--- extensions/mssql/src/models/virtualFKMap.ts
+++ extensions/mssql/src/models/virtualFKMap.ts
@@ -16,14 +16,159 @@ export interface ForeignKeyMapping {
 export let cachedFKMap: Record<string, ForeignKeyMapping> = {};
 
 const dummyFKMap: Record<string, ForeignKeyMapping> = {
-    EmployeeInChargeID: {
-        targetTable: "tblEmployee",
-        targetColumn: "EmployeeID",
+    AssignmentID: {
+        targetTable: "tblAssignment",
+        targetColumn: "AssignmentID",
+        isQuoteRequired: false,
+    },
+    AssignmentNoteID: {
+        targetTable: "tblAssignmentNote",
+        targetColumn: "AssignmentNoteID",
+        isQuoteRequired: false,
+    },
+    AssignmentObjectID: {
+        targetTable: "tblAssignmentObject",
+        targetColumn: "AssignmentObjectID",
+        isQuoteRequired: false,
+    },
+    AssignmentStatusID: {
+        targetTable: "tblAssignmentStatus",
+        targetColumn: "AssignmentStatusID",
+        isQuoteRequired: false,
+    },
+    AssignmentTypeID: {
+        targetTable: "tblAssignmentType",
+        targetColumn: "AssignmentTypeID",
         isQuoteRequired: false,
     },
     CustomerID: {
         targetTable: "tblCustomer",
         targetColumn: "CustomerID",
+        isQuoteRequired: false,
+    },
+    CustomerSiteID: {
+        targetTable: "tblCustomerSite",
+        targetColumn: "CustomerSiteID",
+        isQuoteRequired: false,
+    },
+    DepartmentID: {
+        targetTable: "tblDepartment",
+        targetColumn: "DepartmentID",
+        isQuoteRequired: false,
+    },
+    DimensionID: {
+        targetTable: "tblDimension",
+        targetColumn: "DimensionID",
+        isQuoteRequired: false,
+    },
+    EditAssignmentProductID: {
+        targetTable: "tblAssignmentProduct",
+        targetColumn: "AssignmentProductID",
+        isQuoteRequired: false,
+    },
+    EmployeeInChargeID: {
+        targetTable: "tblEmployee",
+        targetColumn: "EmployeeID",
+        isQuoteRequired: false,
+    },
+    FromEmployeeID: {
+        targetTable: "tblEmployee",
+        targetColumn: "EmployeeID",
+        isQuoteRequired: false,
+    },
+    FromStorageID: {
+        targetTable: "tblStorage",
+        targetColumn: "StorageID",
+        isQuoteRequired: false,
+    },
+    ObjectID: {
+        targetTable: "tblObject",
+        targetColumn: "ObjectID",
+        isQuoteRequired: false,
+    },
+    ObjectTypeID: {
+        targetTable: "tblObjectType",
+        targetColumn: "ObjectTypeID",
+        isQuoteRequired: false,
+    },
+    OfficeClerkID: {
+        targetTable: "tblEmployee",
+        targetColumn: "EmployeeID",
+        isQuoteRequired: false,
+    },
+    OrderID: {
+        targetTable: "tblOrder",
+        targetColumn: "OrderID",
+        isQuoteRequired: false,
+    },
+    OrderItemID: {
+        targetTable: "tblOrderItem",
+        targetColumn: "OrderItemID",
+        isQuoteRequired: false,
+    },
+    ParentAssignmentProductID: {
+        targetTable: "tblAssignmentProduct",
+        targetColumn: "AssignmentProductID",
+        isQuoteRequired: false,
+    },
+    ParentDimensionID: {
+        targetTable: "tblDImension",
+        targetColumn: "DimensionID",
+        isQuoteRequired: false,
+    },
+    ParentObjectID: {
+        targetTable: "tblObject",
+        targetColumn: "ParentObjectID",
+        isQuoteRequired: false,
+    },
+    PocketPCID: {
+        targetTable: "tblPocketPC",
+        targetColumn: "PocketPCID",
+        isQuoteRequired: false,
+    },
+    ProductID: {
+        targetTable: "tblProduct",
+        targetColumn: "ProductID",
+        isQuoteRequired: false,
+    },
+    QualityControlID: {
+        targetTable: "tblQualityControl",
+        targetColumn: "QualityControlID",
+        isQuoteRequired: false,
+    },
+    ResponsibleEmployeeID: {
+        targetTable: "tblEmployee",
+        targetColumn: "EmployeeID",
+        isQuoteRequired: false,
+    },
+    ServiceID: {
+        targetTable: "tblService",
+        targetColumn: "ServiceID",
+        isQuoteRequired: false,
+    },
+    SignatureID: {
+        targetTable: "tblSignature",
+        targetColumn: "SignatureID",
+        isQuoteRequired: false,
+    },
+    StorageID: {
+        targetTable: "tblStorage",
+        targetColumn: "StorageID",
+        isQuoteRequired: false,
+    },
+    SupplierID: {
+        targetTable: "tblSupplier",
+        targetColumn: "SupplierID",
+        isQuoteRequired: false,
+    },
+    ToEmployeeID: {
+        targetTable: "tblEmployee",
+        targetColumn: "EmployeeID",
+        isQuoteRequired: false,
+    },
+    UserProfileID: {
+        targetTable: "tblUserProfile",
+        targetColumn: "UserProfileID",
         isQuoteRequired: false,
     },
 };
